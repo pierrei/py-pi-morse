fd48c028b8eddc18154fbc976ea608b5ce1b46cd
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2018-08-25 22:24:00 +0200

    Adding string matcher

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ from time import sleep
 from morse import Morse
 
 
-def callback(char):
-    sys.stdout.write(char)
-    sys.stdout.flush()
+class StringMatcher(object):
+    string = ''
+    max_length = 5
+
+    def __init__(self, string_to_match, matching_callback):
+        self.string_to_match = string_to_match
+        self.matching_callback = matching_callback
+
+    def new_char(self, char):
+        self.string += char
+        if len(self.string) > self.max_length:
+            self.string = self.string[-self.max_length:]
+
+        if self.string_to_match == self.string:
+            self.matching_callback()
+
+
+def match():
+    print "Match!"
+
+
+string_matcher = StringMatcher("CODE", match)
 
 
 PIN = 3
-morse = Morse(PIN, callback)
+morse = Morse(PIN, string_matcher.new_char)
 while True:
     morse.read_input()
     sleep(0.01)
